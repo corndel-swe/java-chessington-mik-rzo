0ba3247c2ee572e06f35ace1fe3da9bc1b0847b8
mik-rzo <michael.panong3@gmail.com>
2024-09-23 15:45:55 +0100

    implements getAllowedMoves method for Pawn

--- src/main/java/com/corndel/chessington/model/pieces/Pawn.java
+++ src/main/java/com/corndel/chessington/model/pieces/Pawn.java
@@ -36,12 +36,64 @@ public class Pawn implements Piece {
   public List<Move> getAllowedMoves(Coordinates from, Board board) {
     var allowedMoves = new ArrayList<Move>();
     if (getColour().equals(PlayerColour.WHITE)) {
-      allowedMoves.add(new Move(from, from.plus(-1, 0)));
+      // pawn reaches end of board
+      if (from.getRow() == 0) {
+        return allowedMoves;
+      }
+      // no pieces directly in front
+      if (board.get(from.plus(-1, 0)) == null) {
+        allowedMoves.add(new Move(from, from.plus(-1, 0)));
+      }
+      // start position of pawn
+      if (from.getRow() == 6) {
+        // no pieces two squares in front
+        if (board.get(from.plus(-2, 0)) == null) {
+          allowedMoves.add(new Move(from, from.plus(-2, 0)));
+        }
+      }
+      // can move diagonally if enemy piece is there and not going out of bounds
+      if (from.getCol() != 7) {
+        if (board.get(from.plus(-1, 1)) != null) {
+          if (board.get(from.plus(-1, 1)).getColour() != PlayerColour.WHITE) {
+            allowedMoves.add(new Move(from, from.plus(-1, 1)));
+          }
+        }
+      }
+      if (from.getCol() != 0) {
+        if (board.get(from.plus(-1, -1)) != null) {
+          if (board.get(from.plus(-1, -1)).getColour() != PlayerColour.WHITE) {
+            allowedMoves.add(new Move(from, from.plus(-1, -1)));
+          }
+        }
+      }
     } else {
-      allowedMoves.add(new Move(from, from.plus(1, 0)));
+      if (from.getRow() == 7) {
+        return allowedMoves;
+      }
+      if (board.get(from.plus(1, 0)) == null && from.getRow() != 7) {
+        allowedMoves.add(new Move(from, from.plus(1, 0)));
+      }
+      if (from.getRow() == 1) {
+        if (board.get(from.plus(2, 0)) == null) {
+          allowedMoves.add(new Move(from, from.plus(2, 0)));
+        }
+      }
+      if (from.getCol() != 7) {
+        if (board.get(from.plus(1, 1)) != null) {
+          if (board.get(from.plus(1, 1)).getColour() != PlayerColour.BLACK) {
+            allowedMoves.add(new Move(from, from.plus(1, 1)));
+          }
+        }
+      }
+      if (from.getCol() != 0) {
+        if (board.get(from.plus(1, -1)) != null) {
+          if (board.get(from.plus(1, -1)).getColour() != PlayerColour.BLACK) {
+            allowedMoves.add(new Move(from, from.plus(1, -1)));
+          }
+        }
+      }
     }
 
-    // TODO Implement this!
-    return List.of();
+    return allowedMoves;
   }
 }
