53d8e185c00776cb3d8dccc5857b119f6ce10776
mik-rzo <michael.panong3@gmail.com>
2024-09-23 11:12:19 +0100

    completes day 1 exercise 2

--- src/main/java/com/corndel/exercises/Counter.java
+++ src/main/java/com/corndel/exercises/Counter.java
@@ -6,4 +6,16 @@ public class Counter {
   public Counter() {
     this.count = 0;
   }
+
+  public void increment() {
+    this.count++;
+  }
+
+  public void reset() {
+    this.count = 0;
+  }
+
+  public int getCount() {
+    return this.count;
+  }
 }
