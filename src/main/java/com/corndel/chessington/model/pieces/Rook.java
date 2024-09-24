f69efecec96c6297d25877d3996a2ca8d1bd08fa
mik-rzo <michael.panong3@gmail.com>
2024-09-24 15:57:52 +0100

    refactors Rook to inherit from AbstractPiece

--- src/main/java/com/corndel/chessington/model/pieces/Rook.java
+++ src/main/java/com/corndel/chessington/model/pieces/Rook.java
@@ -6,29 +6,10 @@ import com.corndel.chessington.model.Move;
 import com.corndel.chessington.model.PlayerColour;
 import java.util.List;
 
-public class Rook implements Piece {
-
-  private final Piece.PieceType type;
-  protected final PlayerColour colour;
+public class Rook extends AbstractPiece {
 
   public Rook(PlayerColour colour) {
-    this.type = PieceType.ROOK;
-    this.colour = colour;
-  }
-
-  @Override
-  public Piece.PieceType getType() {
-    return type;
-  }
-
-  @Override
-  public PlayerColour getColour() {
-    return colour;
-  }
-
-  @Override
-  public String toString() {
-    return colour.toString() + " " + type.toString();
+    super(PieceType.ROOK, colour);
   }
 
   @Override
