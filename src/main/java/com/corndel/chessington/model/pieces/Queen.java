08fe536f9bdb5ce3d566cffee236d9b628872279
mik-rzo <michael.panong3@gmail.com>
2024-09-26 14:34:23 +0100

    refactors Queen to inherit from AbstractPiece

--- src/main/java/com/corndel/chessington/model/pieces/Queen.java
+++ src/main/java/com/corndel/chessington/model/pieces/Queen.java
@@ -6,32 +6,12 @@ import com.corndel.chessington.model.Move;
 import com.corndel.chessington.model.PlayerColour;
 import java.util.List;
 
-public class Queen implements Piece {
-
-  private final Piece.PieceType type;
-  protected final PlayerColour colour;
+public class Queen extends AbstractPiece {
 
   public Queen(PlayerColour colour) {
-    this.type = PieceType.QUEEN;
-    this.colour = colour;
+    super(PieceType.QUEEN, colour);
   }
 
-  @Override
-  public Piece.PieceType getType() {
-    return type;
-  }
-
-  @Override
-  public PlayerColour getColour() {
-    return colour;
-  }
-
-  @Override
-  public String toString() {
-    return colour.toString() + " " + type.toString();
-  }
-
-  @Override
   public List<Move> getAllowedMoves(Coordinates from, Board board) {
     // TODO Implement this!
     return List.of();
