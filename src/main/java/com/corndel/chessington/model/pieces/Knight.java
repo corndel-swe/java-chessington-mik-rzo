c84caab3476851b6e292f64bb3b5fba2ad538709
mik-rzo <michael.panong3@gmail.com>
2024-09-26 13:35:37 +0100

    adds comments for Knight

--- src/main/java/com/corndel/chessington/model/pieces/Knight.java
+++ src/main/java/com/corndel/chessington/model/pieces/Knight.java
@@ -18,19 +18,25 @@ public class Knight extends AbstractPiece {
     var allowedMoves = new ArrayList<Move>();
 
     for (int i = -2; i < 3; i++) {
+      // skip current knight coordinate
       if (i == 0) {
         continue;
+      // check if going out of bounds
       } else if (from.getRow() + i < 0 || from.getRow() + i > 7) {
         continue;
       }
       for (int j = -2; j < 3; j++) {
+        // skip current knight coordinate
         if (j == 0) {
           continue;
+        // check if going out of bounds
         } else if (from.getCol() + j < 0 || from.getCol() + j > 7) {
           continue;
+        // does knight move in L shape (two squares then one square)
         } else if (Math.abs(i) + Math.abs(j) != 3) {
           continue;
         }
+        // ensures knight doesn't land on friendly piece
         if (board.get(from.plus(i, j)) != null) {
           if (getColour() == board.get(from.plus(i, j)).getColour()) {
             continue;
