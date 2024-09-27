2996ba7c85c87ace1ddab270443174a3a1375c25
mik-rzo <michael.panong3@gmail.com>
2024-09-27 11:54:57 +0100

    refactors getAllowedMoves for Pawn

--- src/main/java/com/corndel/chessington/model/pieces/Pawn.java
+++ src/main/java/com/corndel/chessington/model/pieces/Pawn.java
@@ -15,61 +15,39 @@ public class Pawn extends AbstractPiece {
 
   public List<Move> getAllowedMoves(Coordinates from, Board board) {
     var allowedMoves = new ArrayList<Move>();
-    if (getColour().equals(PlayerColour.WHITE)) {
-      // pawn reaches end of board
-      if (from.getRow() == 0) {
-        return allowedMoves;
-      }
-      // no pieces directly in front
-      if (board.get(from.plus(-1, 0)) == null) {
-        allowedMoves.add(new Move(from, from.plus(-1, 0)));
-      }
-      // start position of pawn
-      if (from.getRow() == 6) {
-        // no pieces two squares in front
-        if (board.get(from.plus(-2, 0)) == null) {
-          allowedMoves.add(new Move(from, from.plus(-2, 0)));
+
+    int direction = getColour().equals(PlayerColour.WHITE) ? -1 : 1;
+
+    // pawn reaches end of board
+    boolean whitePawnTopOfBoard = direction == -1 && from.getRow() == 0;
+    boolean blackPawnBottomOfBoard = direction == 1 && from.getRow() == 7;
+    if (whitePawnTopOfBoard || blackPawnBottomOfBoard) {
+      return allowedMoves;
+    }
+    // no pieces in front of pawn
+    if (board.get(from.plus(direction, 0)) == null) {
+      allowedMoves.add(new Move(from, from.plus(direction, 0)));
+    }
+
+    // start position of pawn
+    boolean startingPositionWhite = direction == -1 && from.getRow() == 6;
+    boolean startingPositionBlack = direction == 1 && from.getRow() == 1;
+    if (startingPositionBlack || startingPositionWhite) {
+      allowedMoves.add(new Move(from, from.plus(direction * 2, 0)));
+    }
+
+    // can capture diagonally
+    if (from.getCol() - 1 >= 0) {
+      if (board.get(from.plus(direction, -1)) != null) {
+        if (getColour() != board.get(from.plus(direction, -1)).getColour()) {
+          allowedMoves.add(new Move(from, from.plus(direction, -1)));
         }
       }
-      // can move diagonally if enemy piece is there and not going out of bounds
-      if (from.getCol() != 7) {
-        if (board.get(from.plus(-1, 1)) != null) {
-          if (board.get(from.plus(-1, 1)).getColour() != PlayerColour.WHITE) {
-            allowedMoves.add(new Move(from, from.plus(-1, 1)));
-          }
-        }
-      }
-      if (from.getCol() != 0) {
-        if (board.get(from.plus(-1, -1)) != null) {
-          if (board.get(from.plus(-1, -1)).getColour() != PlayerColour.WHITE) {
-            allowedMoves.add(new Move(from, from.plus(-1, -1)));
-          }
-        }
-      }
-    } else {
-      if (from.getRow() == 7) {
-        return allowedMoves;
-      }
-      if (board.get(from.plus(1, 0)) == null && from.getRow() != 7) {
-        allowedMoves.add(new Move(from, from.plus(1, 0)));
-      }
-      if (from.getRow() == 1) {
-        if (board.get(from.plus(2, 0)) == null) {
-          allowedMoves.add(new Move(from, from.plus(2, 0)));
-        }
-      }
-      if (from.getCol() != 7) {
-        if (board.get(from.plus(1, 1)) != null) {
-          if (board.get(from.plus(1, 1)).getColour() != PlayerColour.BLACK) {
-            allowedMoves.add(new Move(from, from.plus(1, 1)));
-          }
-        }
-      }
-      if (from.getCol() != 0) {
-        if (board.get(from.plus(1, -1)) != null) {
-          if (board.get(from.plus(1, -1)).getColour() != PlayerColour.BLACK) {
-            allowedMoves.add(new Move(from, from.plus(1, -1)));
-          }
+    }
+    if (from.getCol() + 1 <= 7) {
+      if (board.get(from.plus(direction, 1)) != null) {
+        if (getColour() != board.get(from.plus(direction, 1)).getColour()) {
+          allowedMoves.add(new Move(from, from.plus(direction, 1)));
         }
       }
     }
